68338fe7fd8365f865bf8e5882c524f9ec1ff548
LabebMatta <34980714+LabebMatta@users.noreply.github.com>
2026-03-12 16:20:32 +0200

    Implement CORS support in CodeApplication
    Added CORS configuration to allow all origins and methods.

--- back-end/src/main/java/com/project/code/CodeApplication.java
+++ back-end/src/main/java/com/project/code/CodeApplication.java
@@ -2,15 +2,26 @@ package com.project.code;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
-@ComponentScan("com.project.code")
 public class CodeApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(CodeApplication.class, args);
-		
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(CodeApplication.class, args);
+    }
 
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("*")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE");
+            }
+        };
+    }
 }
