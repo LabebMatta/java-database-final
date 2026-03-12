512998997dc7eb55c18f14cbbd998ef94385f648
LabebMatta <34980714+LabebMatta@users.noreply.github.com>
2026-03-12 16:24:42 +0200

    Rename purchaseProducts to purchaseProduct in DTO

--- back-end/src/main/java/com/project/code/Model/PlaceOrderRequestDTO.java
+++ back-end/src/main/java/com/project/code/Model/PlaceOrderRequestDTO.java
@@ -9,20 +9,25 @@ public class PlaceOrderRequestDTO {
     private String customerPhone;
     private Long storeId;
     private Double totalPrice;
-    private List<PurchaseProductDTO> purchaseProducts;
+    private List<PurchaseProductDTO> purchaseProduct;
 
     public PlaceOrderRequestDTO() {}
 
     public String getCustomerName() { return customerName; }
     public void setCustomerName(String customerName) { this.customerName = customerName; }
+
     public String getCustomerEmail() { return customerEmail; }
     public void setCustomerEmail(String customerEmail) { this.customerEmail = customerEmail; }
+
     public String getCustomerPhone() { return customerPhone; }
     public void setCustomerPhone(String customerPhone) { this.customerPhone = customerPhone; }
+
     public Long getStoreId() { return storeId; }
     public void setStoreId(Long storeId) { this.storeId = storeId; }
+
     public Double getTotalPrice() { return totalPrice; }
     public void setTotalPrice(Double totalPrice) { this.totalPrice = totalPrice; }
-    public List<PurchaseProductDTO> getPurchaseProducts() { return purchaseProducts; }
-    public void setPurchaseProducts(List<PurchaseProductDTO> purchaseProducts) { this.purchaseProducts = purchaseProducts; }
+
+    public List<PurchaseProductDTO> getPurchaseProducts() { return purchaseProduct; }
+    public void setPurchaseProducts(List<PurchaseProductDTO> purchaseProduct) { this.purchaseProduct = purchaseProduct; }
 }
